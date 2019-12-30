cd4a47e2c3f5dd59867a710dfd3e4b275bb12506
Henrik Binggl <henrik.binggl@gmail.com>
2019-12-30 15:57:13 +0100

    fix: changed connection-string

--- src/Api/Infrastructure/Startup.cs
+++ src/Api/Infrastructure/Startup.cs
@@ -60,12 +60,6 @@ namespace Api.Infrastructure
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BookmarkContext context)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
             app.UseRouting();
             app.UseErrorHandling();
             app.UseJwtAuth();
