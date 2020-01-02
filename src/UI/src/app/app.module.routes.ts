b207768ca491a7e2e40496c1b31d0901915451ef
bihe <henrik@binggl.net>
2020-01-02 16:03:04 +0100

    feat: updates after ng build prod; small UI enhancements

--- src/UI/src/app/app.module.routes.ts
+++ src/UI/src/app/app.module.routes.ts
@@ -1,40 +1,44 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, UrlSegment } from '@angular/router';
+import { RouterModule, Routes, UrlMatchResult, UrlSegment } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+
+// custom matcher
+// match for all URLs starting with 'start' and collect the sub-path in
+// the variable path
+// e.g. /start => path: /
+// e.g. /start/Folder1/Folders2 => path: /Folder1/Folder2
+// e.g. /start/a/b/c/d/e/f/g => path: /a/b/c/d/e/f/g
+export function matchStartAndSubPath ( url: UrlSegment[] ): UrlMatchResult {
+
+  if (url.length === 0) {
+    return null;
+  }
+
+  if (url[0].path === 'start') {
+    let path = '/';
+    if (url.length > 1) {
+      url.forEach((e, i) => {
+        if (e.path !== 'start') {
+          if (!path.endsWith('/')) {
+            path += '/';
+          }
+          path += e.path;
+        }
+      });
+    }
+    return {
+      consumed: url,
+      posParams: {
+        path: new UrlSegment(path, {})
+      }
+    };
+  }
+  return null;
+}
 
 const routes: Routes = [
   { path: '', redirectTo: 'start', pathMatch: 'full' },
-  {
-    // custom matcher
-    // match for all URLs starting with 'start' and collect the sub-path in
-    // the variable path
-    // e.g. /start => path: /
-    // e.g. /start/Folder1/Folders2 => path: /Folder1/Folder2
-    // e.g. /start/a/b/c/d/e/f/g => path: /a/b/c/d/e/f/g
-    matcher: (url) => {
-      if (url[0].path === 'start') {
-        let path = '/';
-        if (url.length > 1) {
-          url.forEach((e, i) => {
-            if (e.path !== 'start') {
-              if (!path.endsWith('/')) {
-                path += '/';
-              }
-              path += e.path;
-            }
-          });
-        }
-        return {
-          consumed: url,
-          posParams: {
-            path: new UrlSegment(path, {})
-          }
-        };
-      }
-      return null;
-    },
-    component: HomeComponent
-  },
+  { matcher: matchStartAndSubPath, component: HomeComponent },
   { path: 'start', component: HomeComponent },
   { path: '**', redirectTo: 'start', }
 ];
